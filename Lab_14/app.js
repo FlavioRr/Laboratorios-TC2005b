dc78997dfe79d2bd5df626303451c089d81c13a1
FlavioRr <A01367631@itesm.mx>
2022-03-08 12:36:09 -0600

    Lab14 cambios en el codigo

--- Lab_14/app.js
+++ Lab_14/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bodyCookie = require('cookie-parser');
 const rutas_capybaras = require('./routes/capybaras.routes');
+const rutas_usuarios = require('./routes/user.routes/')
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -12,18 +14,25 @@ app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser()(
+            app.use(session({
+                secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste',
+                resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+                saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+            }));
+        )
 
-app.use('/capybaras', rutas_capybaras);
+        app.use('/capybaras', rutas_capybaras);
 
-//Middleware
-app.use((request, response, next) => {
-    console.log('Middleware!');
-    next(); //Le permite a la petición avanzar hacia el siguiente middleware
-});
+        //Middleware
+        app.use((request, response, next) => {
+            console.log('Middleware!');
+            next(); //Le permite a la petición avanzar hacia el siguiente middleware
+        });
 
-app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    response.send('¡Hola mundo!'); //Manda la respuesta
-});
+        app.use((request, response, next) => {
+            console.log('Otro middleware!');
+            response.send('¡Hola mundo!'); //Manda la respuesta
+        });
 
-app.listen(3000);
+        app.listen(3000);
