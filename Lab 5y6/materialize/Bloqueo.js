56d6d8551b1ff6162739ab991d78115e764bbbff
FlavioRr <A01367631@itesm.mx>
2022-04-26 09:13:04 -0500

    arreglar el nombre de los files

--- Lab 5y6/materialize/Bloqueo.js
+++
@@ -1,3 +0,0 @@
-const nombre=prompt("ingresa tu nombre");
-const apellido=prompt("ingresa tu apellido");
-console.log(nombre + apellido);
---
+++ Lab_5y6/materialize/Bloqueo.js
@@ -0,0 +1,3 @@
+const nombre=prompt("ingresa tu nombre");
+const apellido=prompt("ingresa tu apellido");
+console.log(nombre + apellido);
